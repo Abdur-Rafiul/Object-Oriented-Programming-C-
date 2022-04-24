fcf8e3c83cacba1ca1d5e3953eeb282f36f7e501
Rafiul-Islam <76603094+Abdur-Rafiul@users.noreply.github.com>
2022-04-24 13:42:06 +0600

    Add files via upload

---
+++ OOP/firstclass.h
@@ -0,0 +1,15 @@
+#ifndef FIRSTCLASS_H
+#define FIRSTCLASS_H
+#include "firstclass.h"
+
+class FirstClass
+{
+    public:
+        FirstClass();
+        void display();
+    protected:
+
+    private:
+};
+
+#endif // FIRSTCLASS_H
---
+++ OOP/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "firstclass.h"
+using namespace std;
+
+int main()
+{
+     FirstClass obj;
+     obj.display();
+}
